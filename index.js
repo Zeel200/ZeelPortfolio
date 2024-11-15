80d6e2f0013915b00cd27b3752eec2bb5c1cb5db
Zeel <pzeel881@gmail.com>
2024-11-14 23:06:47 -0500

    updated

--- index.js
+++ index.js
@@ -4,13 +4,20 @@
 
 function contact(event) {
      event.preventDefault();
-     emailJs
-          .sendForm(
-               'service_uddzo3n',
-               'template_1mwowx7',
-               event.target,
-               '-0iCadj9ZE_Ar500e'
-          ).then(() => {
-               console.log('this worked')
-          })
+     // emailJs
+     //      .sendForm(
+     //           'service_uddzo3n',
+     //           'template_1mwowx7',
+     //           event.target,
+     //           '-0iCadj9ZE_Ar500e'
+     //      ).then(() => {
+     //           console.log('this worked')
+     //      })
+
+     const loading = document.querySelector('.modal-overlay-loading')
+     const success = document.querySelector('.modal-overlay-success')
+     loading.classList += " modal-overlay-visible"
+     setTimeout(() => {
+          console.log('it worked 1')
+     }, 500);
 }
