4997128eed0d82f6c4e814a40b391594cd756709
Zeel <pzeel881@gmail.com>
2024-11-15 19:25:36 -0500

    updated

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ function contact(event) {
      event.preventDefault();
      const loading = document.querySelector('.modal-overlay-loading')
      const success = document.querySelector('.modal-overlay-success')
-
-     emailJs
+     loading.classList += " modal-overlay-visible"
+     emailjs
           .sendForm(
                'service_uddzo3n',
                'template_1mwowx7',
                event.target,
                '-0iCadj9ZE_Ar500e'
           ).then(() => {
-               // throw new Error("error")
                loading.classList.remove("modal-overlay-visible")
                success.classList += " modal-overlay-visible"
           }).catch(() => {
@@ -19,10 +18,14 @@ function contact(event) {
                     "The email service is temporarily unavailable. Please contact me directly at my email zeelp2026@gmail.com"
                )
           })
+}
 
-     loading.classList += " modal-overlay-visible"
-     setTimeout(() => {
-
-          console.log('it worked 1')
-     }, 1000);
+let isModalOpen = false
+function toggleModal() {
+     if (isModalOpen) {
+          isModalOpen = false
+          return document.body.classList.remove('modal-open')
+     }
+     isModalOpen = true
+     document.body.classList += " modal-open"
 }
