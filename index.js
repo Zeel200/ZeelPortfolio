c48aa1cb25da8cfe90d589def488372cd9195e31
Zeel <pzeel881@gmail.com>
2024-11-17 16:27:45 -0500

    working on update

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+let isModalOpen = false
+let contrastToggle = false
+const scaleFactor = 1/20
+
+function moveBackground(event) {
+     const shapes = document.querySelectorAll(".shape")
+     const x = event.clientX * scaleFactor
+     const y = event.clientY * scaleFactor
+     
+     for (let i = 0; i < shapes.length; i++) {
+          const isOdd = i % 2 !== 0
+          const boolInt = isOdd ? -1 : 1
+          shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+     }
+}
+
+function toggleContrast() {
+     contrastToggle = !contrastToggle
+
+     if (contrastToggle) {
+     document.body.classList += " dark-theme"
+     }
+
+     else {
+          document.body.classList.remove("dark-theme")
+     }
+}
+
 function contact(event) {
      event.preventDefault();
      const loading = document.querySelector('.modal-overlay-loading')
@@ -20,7 +48,6 @@ function contact(event) {
           })
 }
 
-let isModalOpen = false
 function toggleModal() {
      if (isModalOpen) {
           isModalOpen = false
